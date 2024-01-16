4e70955428703f5168ea5d0453690ee9f6c7d589
Brotherick <full8back@gmail.com>
2024-01-16 11:25:25 +0300

    separated the controller from the service

--- src/main/java/org/food/security/service/AuthenticationService.java
+++ src/main/java/org/food/security/service/AuthenticationService.java
@@ -2,12 +2,13 @@ package org.food.security.service;
 
 import lombok.RequiredArgsConstructor;
 import org.food.exception.classes.TokenRefreshException;
-import org.food.security.dto.UserInfo;
+import org.food.security.dto.UserDto;
 import org.food.security.model.RefreshToken;
 import org.food.security.model.Role;
 import org.food.security.model.User;
 
 import org.food.security.payload.request.RefreshTokenRequest;
+import org.food.security.payload.responce.JwtAuthenticationResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -36,7 +37,7 @@ public class AuthenticationService {
      * @param request данные пользователя
      * @return токен
      */
-    public ResponseEntity signUp(SignUpRequest request) {
+    public JwtAuthenticationResponse signUp(SignUpRequest request) {
 
         var user = User.builder()
                 .username(request.getUsername())
@@ -50,12 +51,12 @@ public class AuthenticationService {
         String accessToken = jwtService.generateToken(user);
         String refreshToken = refreshTokenService.createRefreshToken(request).getToken();
 
-        return ResponseEntity.ok().header("AccessToken", accessToken)
-                .header("RefreshToken", refreshToken)
-                .body(new UserInfo(user.getId(),
-                        user.getUsername(),
-                        user.getEmail(),
-                        user.getRole()));
+        JwtAuthenticationResponse response = JwtAuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .userName(user.getUsername())
+                .build();
+        return response;
     }
 
     /**
@@ -64,7 +65,7 @@ public class AuthenticationService {
      * @param loginRequest данные пользователя
      * @return токен
      */
-    public ResponseEntity signIn(SignInRequest loginRequest) {
+    public JwtAuthenticationResponse signIn(SignInRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                     loginRequest.getUsername(),
                     loginRequest.getPassword()
@@ -72,27 +73,30 @@ public class AuthenticationService {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        var user = userService
-                .userDetailsService()
-                .loadUserByUsername(loginRequest.getUsername());
+        var user = userService.getByUsername(loginRequest.getUsername());
 
-        var jwt = jwtService.generateToken(user);
-        return ResponseEntity.ok().build();
+        var accessToken = jwtService.generateToken(user);
+
+        JwtAuthenticationResponse response = JwtAuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .build();
+        return response;
     }
 
-    public ResponseEntity refreshToken(RefreshTokenRequest request){
+    public JwtAuthenticationResponse refreshToken(RefreshTokenRequest request){
         Optional<?> refreshedToken = refreshTokenService.findByToken(request.getRefreshToken());
         if (refreshedToken != null) {
-            User user = userService.getByUsername(request.getUserInfo().getUsername());
+            User user = userService.getByUsername(request.getUserDto().getUsername());
 
             String newAuthToken = jwtService.generateToken(user);
             RefreshToken newRefreshToken = refreshTokenService.createRefreshToken(user);
-
-            return ResponseEntity.ok().header("accessToken", newAuthToken)
-                    .header("refreshToken", newRefreshToken.getToken())
+            JwtAuthenticationResponse response = JwtAuthenticationResponse.builder()
+                    .accessToken(newAuthToken)
+                    .refreshToken(newRefreshToken.getToken())
                     .build();
+            return response;
         } else {
-            throw new TokenRefreshException(request.getUserInfo().getUsername(), ":RefreshTokenError");
+            throw new TokenRefreshException(request.getUserDto().getUsername(), ":RefreshTokenError");
         }
     }
 }
