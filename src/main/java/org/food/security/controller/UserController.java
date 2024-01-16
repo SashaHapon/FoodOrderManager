b597a10383f6adba6d08811d7b6ad87eceb7cfd7
Brotherick <full8back@gmail.com>
2024-01-16 11:29:32 +0300

    code refactor

--- src/main/java/org/food/security/controller/UserController.java
+++ src/main/java/org/food/security/controller/UserController.java
@@ -1,10 +1,9 @@
 package org.food.security.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.food.security.dto.UserInfo;
+import org.food.security.dto.UserDto;
 import org.food.security.model.User;
 import org.food.security.service.UserService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +13,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{name}")
-    public UserInfo getUser(@PathVariable("name") String name ){
-        User user = userService.getByUsername(name);
-        return new UserInfo(user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getRole());
+    public UserDto getUser(@PathVariable("name") String name ){
+        UserDto user = userService.getUserDtoByUsername(name);
+        return user;
     }
 }
