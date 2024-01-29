b0b47b7b1e98bf01ba3e5fb64b78ebf1b94f5a30
Alexandr Hapon <full8back@gmail.com>
2024-01-29 10:59:02 +0300

    #19 add integration test for account

---
+++ src/test/java/org/food/integration/AccountIntegrationTest.java
@@ -0,0 +1,15 @@
+package org.food.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccountIntegrationTest {
+
+    @BeforeEach
+    public void init(){
+
+    }
+
+
+}
