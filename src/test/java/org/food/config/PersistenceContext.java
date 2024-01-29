7636e347467a1e6b7c4b6f6a9be2fe9e4814a4dd
Alexandr Hapon <full8back@gmail.com>
2024-01-29 21:37:05 +0300

    #19 add MockBeans

--- src/test/java/org/food/config/PersistenceContext.java
+++ src/test/java/org/food/config/PersistenceContext.java
@@ -1,13 +1,38 @@
 package org.food.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.food.api.repository.AccountRepository;
+import org.food.api.repository.MealRepository;
+import org.food.api.repository.OrderRepository;
+import org.food.security.repository.RefreshTokenRepository;
+import org.food.security.repository.UserRepository;
+import org.food.security.service.UserService;
+import org.h2.tools.Server;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import java.sql.SQLException;
 
-@Configuration
+@TestConfiguration
+@ComponentScan(basePackages = "src/test/java/org/food")
 public class PersistenceContext {
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AccountRepository accountRepository;
+
+    @MockBean
+    private MealRepository mealRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @MockBean
+    private RefreshTokenRepository refreshTokenRepository;
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
@@ -16,5 +41,10 @@ public class PersistenceContext {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp","-tcpAllowOthers","-tcpPort","9092");
     }
 }
