d409ba66f5c7a2b4e4c40776c681bbb7ac655d97
Alexandr Hapon <full8back@gmail.com>
2024-02-29 13:03:27 +0300

    #48 pull request fix

--- order-manager/src/main/java/org/food/Application.java
+++ order-manager/src/main/java/org/food/Application.java
@@ -1,13 +1,17 @@
 package org.food;
 
+import feign.Retryer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignClientProperties;
 
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
     public static void main(String[] args) {
+       // FeignClientProperties
         SpringApplication.run(Application.class, args);
     }
 
