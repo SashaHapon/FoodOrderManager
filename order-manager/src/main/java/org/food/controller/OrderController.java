25c2c90024e7f0523a048d2bc412eab05d09f79e
Alexandr Hapon <full8back@gmail.com>
2024-03-06 10:14:53 +0300

    #52 fix

--- order-manager/src/main/java/org/food/controller/OrderController.java
+++ order-manager/src/main/java/org/food/controller/OrderController.java
@@ -1,6 +1,5 @@
 package org.food.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
