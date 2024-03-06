6f9554eaaa7071767f8e359eea2be4f9865e4911
Alexandr Hapon <full8back@gmail.com>
2024-03-06 14:15:40 +0300

    #master fix

--- receipt-service/src/main/java/org/receipt/service/impl/ReceiptServiceImpl.java
+++ receipt-service/src/main/java/org/receipt/service/impl/ReceiptServiceImpl.java
@@ -5,31 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.receipt.model.Receipt;
 import org.receipt.service.ReceiptService;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.StringTemplateResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ReceiptServiceImpl implements ReceiptService {
-
-    private final TemplateEngine templateEngine;
+    private final SpringTemplateEngine springTemplateEngine;
 
     @Override
     public Receipt print(Receipt receipt) {
-        StringTemplateResolver resolver = new StringTemplateResolver();
-        resolver.setTemplateMode(TemplateMode.HTML);
-        templateEngine.setTemplateResolver(resolver);
-
         Context context = new Context();
         context.setVariable("localDateTimes", receipt.getLocalDateTimes());
         context.setVariable("orderId", receipt.getOrderId());
         context.setVariable("items", receipt.getItems());
         context.setVariable("orderTotalCost", receipt.getOrderTotalCost());
 
-        String formattedText = templateEngine.process("receipt_template", context);
+        String formattedText = springTemplateEngine.process("receipt_template", context);
         log.info(formattedText);
         receipt.setText(formattedText);
         return receipt;
